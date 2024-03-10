f1cd6bcd9b9decccd5325254ac9140ad011107ea
PRATHAM DILIP GUPTA <135963325+CoolboiVlogs@users.noreply.github.com>
2024-03-10 14:19:30 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 7032024 
+let accountEmail = "npg0703@gmail.com"
+var accountPassword = "05110208"
+accountCity = "Mumbai"
+let accountState;
+
+accountEmail = "npg070324@gmail.com"
+accountPassword = "02080511"
+accountCity = "Dadar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
