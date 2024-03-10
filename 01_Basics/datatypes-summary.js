e86b19d6ed887d770f51bbede6d391b6f857f79f
PRATHAM DILIP GUPTA <135963325+CoolboiVlogs@users.noreply.github.com>
2024-03-10 14:38:50 +0000

    DT Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1603')
+const anotherId = Symbol('1603')
+
+console.log(id === anotherId);
+
+const family = ["Pratham", "Shrishti", "Manisha"];
+let myObj = {
+    name: "Pratham Gupta",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello Ji");
+}
+
+console.log(typeof anotherId);
