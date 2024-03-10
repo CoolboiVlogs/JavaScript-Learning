8bc87fae93ce95d980f02841720422319c019657
PRATHAM DILIP GUPTA <135963325+CoolboiVlogs@users.noreply.github.com>
2024-03-10 14:25:32 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(3 
+    +
+     3)
+
+console.log("Pratham Gupta")
+
+let name = "Pratham"
+let age = 20
+let isLoggedIn = false
+let state;
+
+console.log(typeof undefined);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
