3e0bf33f8f98275a656caecd480e374de8b803f3
PRATHAM DILIP GUPTA <135963325+CoolboiVlogs@users.noreply.github.com>
2024-03-17 12:24:51 +0000

    Strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "PRATHAM GUPTA"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('PRATHAM_GUPTA-pg-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,14 +22,14 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   PRATHAM GUPTA    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://github.com/CoolboiVlogs"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('PRATHAM'))
 
 console.log(gameName.split('-'));
